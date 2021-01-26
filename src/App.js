e253046410e34971ac99a46d2d750865a3b01706
naderramez <naderramez11@gmail.com>
2021-01-26 20:38:05 +0200

    create react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className="display-1 text-center">
+        Test Bootstrap
+      </div>
+    </div>
+  );
+}
+
+export default App;
