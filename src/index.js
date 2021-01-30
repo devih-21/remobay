a70e9f30b454a05b19f482b3dd96386ea8f85929
yasseressam123 <yasseressam123@yahoo.com>
2021-01-30 03:02:17 +0200

    last edit

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "../node_modules/font-awesome/css/font-awesome.css";
-import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import "../node_modules/jquery/dist/jquery";
 import "../node_modules/popper.js/dist/popper-utils";
 import "../node_modules/bootstrap/js/src/modal"
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import '../node_modules/bootstrap/dist/js/bootstrap.js';
 
 
 
